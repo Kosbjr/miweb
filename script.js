cbee6a1efcd335f1e5c70c1e9a140d8301f27a99
Kosbjr <hectorgarcia05.hgg@gmail.com>
2026-01-13 12:15:34 +0100

    Simplificación y cambios de la web

--- script.js
+++ script.js
@@ -6,6 +6,15 @@ function scrollInfo() {
   document.getElementById('infoBox').scrollIntoView({ behavior: 'smooth' });
 }
 window.onload = function(){
+  let imagenes =["imagenes/img1.png","imagenes/img2.png","imagenes/img3.png"];
+  let indice = 0;
+  let imgs = document.getElementById("imgs");
+
+  this.setInterval(()=> {
+    indice = (indice+1) % imagenes.length;
+    imgs.src = imagenes[indice];
+  },5000);
+
   const saludo = document.getElementById("saludo");
   const inpNombre = document.getElementById("nombrecito");
   const btnSaludo = document.getElementById("btnSaludo");
