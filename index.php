2b44f9da700534430739a139681bb7bd21945050
Héctor <hectorgarcia05.hgg@gmail.com>
2026-01-24 01:32:59 +0100

    Cambio a solo un Archivo

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
+<?php
+// Variable para el saludo
+$saludo = "";
+
+// Si viene un POST, generamos el saludo
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && !empty($_POST['nombre'])) {
+    $nombre = htmlspecialchars($_POST['nombre']);
+    $saludo = "¡Hola $nombre, Bienvenido a Mi Web!";
+} elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $saludo = "Introduce un nombre válido";
+}
+?>
 <!doctype html>
 <html lang="es">
 <head>
@@ -6,32 +18,27 @@
   <title>Bienvenidos a mi Página</title>
   <link rel="stylesheet" href="style.css">
 </head>
-
 <body>
 
-  <div class="card">
-    <img src="imagenes/img1.png" alt="" id="imgs">
-    <h1>Bienvenidos a mi web</h1>
+<div class="card">
+  <img src="imagenes/img1.png" alt="" id="imgs">
+  <h1>Bienvenidos a mi web</h1>
 
-    <div class="form-group">
-      <form action="saludo.php" method="post">
-        <input type="text" name="nombre">
-        <input type="submit" name="enviar" value="Enviar">
-      </form>
-    </div>
-
-    <p id="saludo">
-  <?php
-    if (isset($_GET['saludo'])) {
-        echo $_GET['saludo'];
-    }
-  ?>
-</p>
-    <p>Última actualización: <strong id="time"></strong></p>
-    <br>
-    
+  <div class="form-group">
+    <form method="post" action="">
+      <input type="text" name="nombre" required>
+      <input type="submit" value="Enviar">
+    </form>
   </div>
 
-  <script src="script.js"></script>
+  <p id="saludo">
+    <?php echo $saludo; ?>
+  </p>
+
+  <p>Última actualización: <strong id="time"></strong></p>
+  <br>
+</div>
+
+<script src="script.js"></script>
 </body>
 </html>
